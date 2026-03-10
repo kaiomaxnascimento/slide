ad2a75f3261803b0a1b7f5659e36d5af0446aed1
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-10 19:25:01 -0300

    esqueleto dos eventos do slider

---
+++ js/script.js
@@ -0,0 +1,5 @@
+import Slider from './slide.js';
+
+const slider = new Slider('.slider', '.slider-wrapper');
+
+slider.init();
---
+++ js/slide.js
@@ -0,0 +1,35 @@
+export default class Slider {
+  constructor(slider, wrapper) {
+    this.slider = document.querySelector(slider);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
+
+  onMove(event) {}
+
+  onEnd(event) {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+  }
+
+  addSliderEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onMove = this.onMove.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSliderEvents();
+    return this;
+  }
+}
